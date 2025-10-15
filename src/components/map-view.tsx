5c0d6a402b95a11b4373115d4c7a96a029e00cf5
Tushar Jaisi <tusharjaisi2006@gmail.com>
2025-10-15 18:39:50 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/map-view.tsx
+++ src/components/map-view.tsx
@@ -1,18 +1,34 @@
 'use client';
 
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { useEffect } from 'react';
+
+// This component will handle map view changes
+function MapUpdater({ center, zoom }: { center: [number, number]; zoom: number }) {
+  const map = useMap();
+  useEffect(() => {
+    map.setView(center, zoom);
+  }, [center, zoom, map]);
+  return null;
+}
 
 interface MapViewProps {
   center: [number, number];
   zoom: number;
 }
 
-// Since react-leaflet is client-side only, we check for window
 export default function MapView({ center, zoom }: MapViewProps) {
-  if (typeof window === 'undefined') {
-    return null;
-  }
+    // Fix for broken marker icon in Next.js
+    useEffect(() => {
+        delete (L.Icon.Default.prototype as any)._getIconUrl;
+        L.Icon.Default.mergeOptions({
+            iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+            iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+            shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+        });
+    }, []);
 
   return (
     <MapContainer
@@ -20,11 +36,14 @@ export default function MapView({ center, zoom }: MapViewProps) {
       zoom={zoom}
       scrollWheelZoom={true}
       style={{ height: '100%', width: '100%' }}
+      // This prevents the map from being re-initialized
+      whenCreated={() => {}}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <MapUpdater center={center} zoom={zoom} />
     </MapContainer>
   );
 }
